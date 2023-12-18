ddc9aade27236729646812a9e1b9c2d1cb7d85e3
Queje <jeremyquerne@hotmail.fr>
2023-12-18 22:15:17 +0100

    winter exhibition update pictures

--- src/components/layout/CarouselDisplay.jsx
+++ src/components/layout/CarouselDisplay.jsx
@@ -5,7 +5,7 @@ import image3 from 'assets/images/image3.webp';
 import image4 from 'assets/images/image4.webp';
 import image5 from 'assets/images/image5.webp';
 import image6 from 'assets/images/image6.webp';
-import image7 from 'assets/images/image7.webp';
+//import image7 from 'assets/images/image7.webp';
 //import image8 from 'assets/images/image6.webp';
 
 const CarouselDisplay = () => {
@@ -18,11 +18,11 @@ const CarouselDisplay = () => {
         <img
           className="d-block"
           src={image1}
-          alt="salon des arts, été 2023"
+          alt="salon des arts, Hiver 2023"
         />
-        {/* <Carousel.Caption>
-          <h4 className="captiontext">salon des arts, été 2023</h4>
-        </Carousel.Caption> */}
+        <Carousel.Caption>
+          <h4 className="captiontext">Alain Carpentier</h4>
+        </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item className="imagecarousel">
@@ -31,9 +31,9 @@ const CarouselDisplay = () => {
           src={image2}
           alt="salon des arts, été 2023"
         />
-        {/* <Carousel.Caption>
-          <h4 className="captiontext">salon des arts, été 2023</h4>
-        </Carousel.Caption> */}
+        <Carousel.Caption>
+          <h4 className="captiontext">Caroline Lavisse</h4>
+        </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item className="imagecarousel">
@@ -42,9 +42,9 @@ const CarouselDisplay = () => {
           src={image3}
           alt="salon des arts, été 2023"
         />
-        {/* <Carousel.Caption>
-          <h4 className="captiontext">salon des arts, été 2023</h4>
-        </Carousel.Caption> */}
+        <Carousel.Caption>
+          <h4 className="captiontext">Bogdan Tolbariu</h4>
+        </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item className="imagecarousel">
@@ -53,9 +53,9 @@ const CarouselDisplay = () => {
           src={image4}
           alt="salon des arts, été 2023"
         />
-        {/* <Carousel.Caption>
-          <h4 className="captiontext">salon des arts, été 2023</h4>
-        </Carousel.Caption> */}
+        <Carousel.Caption>
+          <h4 className="captiontext">Isablelle D'Arboussier</h4>
+        </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item className="imagecarousel">
@@ -64,21 +64,21 @@ const CarouselDisplay = () => {
           src={image5}
           alt="salon des arts, été 2023"
         />
-        {/* <Carousel.Caption>
-          <h4 className="captiontext">salon des arts, été 2023</h4>
-        </Carousel.Caption> */}
+        <Carousel.Caption>
+          <h4 className="captiontext">Laurence Poidatz</h4>
+        </Carousel.Caption>
       </Carousel.Item>
 
-      <Carousel.Item className="imagecarousel">
+      {/* <Carousel.Item className="imagecarousel">
         <img
           className="d-block"
           src={image7}
           alt="salon des arts, été 2023"
         />
-        {/* <Carousel.Caption>
+        <Carousel.Caption>
           <h4 className="captiontext">salon des arts, été 2023</h4>
-        </Carousel.Caption> */}
-      </Carousel.Item>
+        </Carousel.Caption>
+      </Carousel.Item> */}
 
       <Carousel.Item className="imagecarousel">
         <img
@@ -86,9 +86,9 @@ const CarouselDisplay = () => {
           src={image6}
           alt="salon des arts, été 2023"
         />
-        {/* <Carousel.Caption>
-          <h4 className="captiontext">salon des arts, été 2023</h4>
-        </Carousel.Caption> */}
+        <Carousel.Caption>
+          <h4 className="captiontext">Joëlle Troussier</h4>
+        </Carousel.Caption>
       </Carousel.Item> 
 
     </Carousel>
