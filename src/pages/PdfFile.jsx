eb43e272c2aa61900453085dafba096c6565693b
Queje <jeremyquerne@hotmail.fr>
2024-03-19 18:30:24 +0100

    first update for the 2024 summer exhibition

--- src/pages/PdfFile.jsx
+++ src/pages/PdfFile.jsx
@@ -10,19 +10,19 @@ export class PdfFile extends React.PureComponent {
       <div className="PdfFile">
         <h2 id="formtitle" className="text-center">Salon des Arts - Carantec - Peinture</h2>
         <br></br>
-        <p className="font-weight-bold mt-1">Formulaire d'inscription pour l'exposition du 17/12/2023, Tarif : {REACT_APP_PRICE} </p>
+        <p className="font-weight-bold mt-1">Formulaire d'inscription pour l'exposition du 14/07/2024, Tarif : {REACT_APP_PRICE} </p>
         <p>Surface réservée à chaque artiste : 1m20 sur 1m80 (horizontal ou vertical)</p>
         <br></br>
         <Form>
           <FormGroup className="group1" as={Row}>
             <FormLabel className="label" column sm="2">
-              Prénom :
+              Nom :
             </FormLabel>
             <Col className="textinput" sm="4">
               <FormControl type="text"/>
             </Col>
             <FormLabel className="label" column sm="2">
-              Nom :
+              Prénom :
             </FormLabel>
             <Col className="textinput" sm="4">
               <FormControl type="text"/>
@@ -173,7 +173,7 @@ export class PdfFile extends React.PureComponent {
             <FormCheck inline label="Réalisera une démonstration"/>
             <FormCheck inline label="confirme que mes œuvres exposées sont bien assurées"/>
             <FormCheck inline label="Possibilité de faire un après-midi vente - dédicace de vos livres (prévenir à l’avance pour l’annoncer dans la presse)"/>
-            <FormCheck inline label="Déclare avoir pris connaissance du règlement du salon d’Hiver 2023 de Carantec auquel je souhaite participer et en accepte les clauses"/>
+            <FormCheck inline label="Déclare avoir pris connaissance du règlement du salon d’Eté 2024 de Carantec auquel je souhaite participer et en accepte les clauses"/>
           </FormGroup>
           <FormLabel column sm="2">
             Signature:
