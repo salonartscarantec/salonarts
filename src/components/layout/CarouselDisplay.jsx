bb8fd077969917c7cbf4119254474089d59a83e2
Queje <jeremyquerne@hotmail.fr>
2025-07-16 10:57:07 +0200

    update carousel images for summer exhibition 2025

--- src/components/layout/CarouselDisplay.jsx
+++ src/components/layout/CarouselDisplay.jsx
@@ -5,8 +5,7 @@ import image3 from 'assets/images/image3.webp';
 import image4 from 'assets/images/image4.webp';
 import image5 from 'assets/images/image5.webp';
 import image6 from 'assets/images/image6.webp';
-//import image7 from 'assets/images/image7.webp';
-//import image8 from 'assets/images/image6.webp';
+import image7 from 'assets/images/image7.webp';
 
 const CarouselDisplay = () => {
     
@@ -21,7 +20,7 @@ const CarouselDisplay = () => {
           alt="salon des arts, été 2024"
         />
         <Carousel.Caption>
-          <h4 className="captiontext">Alexis Le Borgne</h4>
+          <h4 className="captiontext">Hélène Matlis</h4>
         </Carousel.Caption>
       </Carousel.Item>
 
@@ -32,7 +31,7 @@ const CarouselDisplay = () => {
           alt="salon des arts, été 2024"
         />
         <Carousel.Caption>
-          <h4 className="captiontext">Charga</h4>
+          <h4 className="captiontext">Max Loriquet</h4>
         </Carousel.Caption>
       </Carousel.Item>
 
@@ -43,7 +42,7 @@ const CarouselDisplay = () => {
           alt="salon des arts, été 2024"
         />
         <Carousel.Caption>
-          <h4 className="captiontext">Christian Sanchez</h4>
+          <h4 className="captiontext">Yves Jehanno</h4>
         </Carousel.Caption>
       </Carousel.Item>
 
@@ -54,7 +53,7 @@ const CarouselDisplay = () => {
           alt="salon des arts, été 2024"
         />
         <Carousel.Caption>
-          <h4 className="captiontext">Jacques De Kerdrel</h4>
+          <h4 className="captiontext">Soisik Oger Le Goff</h4>
         </Carousel.Caption>
       </Carousel.Item>
 
@@ -65,7 +64,7 @@ const CarouselDisplay = () => {
           alt="salon des arts, été 2024"
         />
         <Carousel.Caption>
-          <h4 className="captiontext">Magdeleine Mouchot</h4>
+          <h4 className="captiontext">Evelyne Brunet</h4>
         </Carousel.Caption>
       </Carousel.Item>
 
@@ -76,20 +75,20 @@ const CarouselDisplay = () => {
           alt="salon des arts, été 2024"
         />
         <Carousel.Caption>
-          <h4 className="captiontext">Veronique Jacquelin</h4>
+          <h4 className="captiontext">Tatie Hamon</h4>
         </Carousel.Caption>
       </Carousel.Item>
 
-      {/* <Carousel.Item className="imagecarousel">
+      <Carousel.Item className="imagecarousel">
         <img
           className="d-block"
           src={image7}
           alt="salon des arts, été 2024"
         />
         <Carousel.Caption>
-          <h4 className="captiontext">Sylvie TROUVÉ</h4>
+          <h4 className="captiontext">Chantal Tymen</h4>
         </Carousel.Caption>
-      </Carousel.Item>  */}
+      </Carousel.Item>
 
     </Carousel>
     </>
